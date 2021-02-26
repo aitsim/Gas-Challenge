2a9327e6a1de8b53f6b98c5d5fe90ba46fa7c8bb
aitsim <aitsihamou.med@gmail.com>
2021-02-25 20:26:07 -0500

    monthly prices

--- get_data.py
+++ get_data.py
@@ -26,14 +26,45 @@ def csv_from_excel(file):
     with open('daily_prices.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for i in range(3,10):
+        for i in range(3,200):
             date = sh.cell_value(i, 0)
             price = sh.cell_value(i, 1)
             dt = xlrd.xldate_as_datetime(date, 0)
             date_object = dt.date()
             writer.writerow({'date':date_object, 'price': price})
-            print(date_object,price)
+            # print(date_object,price)
+
+
+def monthly_prices ():
+    fieldnames = ['month', 'price']
+    with open('daily_prices.csv', 'r', newline='') as csvfile:
+        reader  = csv.DictReader(csvfile)
+        m=''
+        d=''
+        i = 0
+        with open('monthly_prices.csv', 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in reader:
+                date = row["date"].split("-")
+                month = date[1]
+                day = date[2]
+                if (i==0):
+                    writer.writerow({'month':row["date"], 'price': row["price"]})
+                    i+=1
+                    last_month = month
+                    continue
+                if (last_month == month):
+                    continue
+                else:
+                    writer.writerow({'month':row["date"], 'price': row["price"]})
+                    last_month = month
+
+
+                
 
 
 
-csv_from_excel(get_xls_file(monthly_url))
+csv_from_excel(get_xls_file(daily_url))
+
+monthly_prices()
