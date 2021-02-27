26f4aab822caa3582a2686cc404f792a6e91ae95
aitsim <aitsihamou.med@gmail.com>
2021-02-27 03:07:39 -0500

    data visualization

--- get_data.py
+++ get_data.py
@@ -1,20 +1,14 @@
-import requests
-import cgi
-import shutil
 import os
-import urllib
-import pandas as pd
+import urllib.request
 import xlrd
 import csv
 
-monthly_url = 'https://www.eia.gov/dnav/ng/hist_xls/RNGWHHDm.xls'
 daily_url = 'https://www.eia.gov/dnav/ng/hist_xls/RNGWHHDd.xls'
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def get_xls_file(url):
     file_name, headers = urllib.request.urlretrieve(url)
     return file_name
-
 
 
 def csv_from_excel(file):
@@ -26,13 +20,12 @@ def csv_from_excel(file):
     with open('daily_prices.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for i in range(3,200):
+        for i in range(3,nrows):
             date = sh.cell_value(i, 0)
             price = sh.cell_value(i, 1)
             dt = xlrd.xldate_as_datetime(date, 0)
             date_object = dt.date()
             writer.writerow({'date':date_object, 'price': price})
-            # print(date_object,price)
 
 
 def monthly_prices ():
